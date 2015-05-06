88dc6f472d06db55c8e0006b28675bf099dfad96
Hans Kirchner <hans-kirchner@gmx.de>
2015-05-06 18:21:23 +0200

    Add support for xboxdrv

--- include/gamepad/gamepad_types.h
+++ include/gamepad/gamepad_types.h
@@ -1,7 +1,7 @@
 #ifndef GAMEPAD_TYPES_H
 #define GAMEPAD_TYPES_H
 #include <gamepad/gamepad.h>
-void setAsXBoxController(Gamepad &pad){
+void setAsXBoxControllerXpad(Gamepad &pad){
     pad.addButton("A",0);
     pad.addButton("B",1);
     pad.addButton("X",2);
@@ -21,4 +21,26 @@ void setAsXBoxController(Gamepad &pad){
     pad.addAxis("LT",2,-1);
     pad.addAxis("RT",5,-1);
 }
+
+void setAsXBoxControllerXboxdrv(Gamepad &pad) {
+    pad.addButton("A",0);
+    pad.addButton("B",1);
+    pad.addButton("X",2);
+    pad.addButton("Y",3);
+    pad.addButton("A1",9);
+    pad.addButton("A3",10);
+    pad.addButton("LB",4);
+    pad.addButton("RB",5);
+    pad.addButton("BACK",6);
+    pad.addButton("START",7);
+    pad.addButton("XBOX",8);
+
+    //add axis
+    pad.addAxis("LS",0,1);
+    pad.addAxis("MX",6,7);
+    pad.addAxis("RS",2,3);
+    pad.addAxis("LT",5,-1);
+    pad.addAxis("RT",4,-1);
+}
+
 #endif
